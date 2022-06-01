00b382539d8e6a5cd0aface71cd6ec8e6711b8af
swati <swati.sbm@gmail.com>
2022-06-01 14:02:41 +0530

    committing new change from intellij

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("adding in master of forked");
+        System.out.println("changes from intellij");
     }
 }
