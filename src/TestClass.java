15d423dfd4e3ee4bb82b2f26ec4bc56cdc9ad625
swatisbm31 <swati.sbm31@gmail.com>
2022-06-01 15:08:51 +0530

    committing new change from intellij

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,7 +1,7 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("somebody changed in upstream.");
-        System.out.println("adding in master of forked");
-        System.out.println("changes from intellij");
+        System.out.println("adding in master of forked1");
+        System.out.println("changes from intellij1");
     }
 }
