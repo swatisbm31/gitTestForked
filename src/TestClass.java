225a19a41224bde5cc7edf84a6e8ef76d83c3cad
swatisbm31 <106470173+swatisbm31@users.noreply.github.com>
2022-06-01 16:20:58 +0530

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,13 +1,13 @@
 public class TestClass {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("somebody changed in upstream1");
         System.out.println("adding in master of forked");
         System.out.println("changes from intellij");
-=======
+
         System.out.println("somebody changed in upstream.");
         System.out.println("adding in master of forked1");
         System.out.println("changes from intellij1");
->>>>>>> 15d423dfd4e3ee4bb82b2f26ec4bc56cdc9ad625
+
     }
 }
